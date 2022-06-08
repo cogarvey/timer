48f22f33e1244a882af49ad5cbf04ef5a3861694
Colleen Garvey <colleenellengarvey@gmail.com>
2022-06-08 09:39:43 -0400

    updated to include functions and if statements

--- timer1.js
+++ timer1.js
@@ -1,9 +1,17 @@
 const args = process.argv;
 
-
-for (let time of args) {
-  setTimeout(() => {
-    console.log(time);
-    process.stdout.write('\x07')
-  }, time * 1000);
+const alarmFunction = function (args) {
+  if (args.length === 0) {
+    return;
+  }
+  for (let time of args) {
+    if (time >= 0 && time !== NaN) {
+      setTimeout(() => {
+        console.log(time);
+        process.stdout.write('\x07')
+      }, time * 1000);
+    }
+  }
 }
+
+alarmFunction(args)
